c61de8dee7208ad4b0b2ea2d458310cf02e6f3fb
Jackie Hudepohl <jhudepohl@onlusr.onl>
2015-06-25 13:16:51 -0500

    output to terminal

--- Virtual_Machines/configVMs.sh
+++ Virtual_Machines/configVMs.sh
@@ -4,7 +4,7 @@ CWD=`pwd`
 source ~/.topology
 source hosts
 
-read -p "VM Password: " -s SSHPASS
+read -p "VM Password: " -s SSHPASS ; echo
 export SSHPASS
 
 for s in "${ROUTER_HOST_PAIRS[@]}" 
@@ -15,5 +15,6 @@ do
   PREFIX=${pair_info[2]}
   ADDRESS=${pair_info[3]}
   
-  sshpass -e ssh -t ${!HOST} "echo $SSHPASS | sudo -S /sbin/route add -net 192.168.0.0/16 gw $ADDRESS" 
+  echo "Configuring IP routing table on $HOST"
+  sshpass -e ssh -t ${!HOST} "echo $SSHPASS | sudo -S -p '' /sbin/route add -net 192.168.0.0/16 gw $ADDRESS" 
 done
